f9c75630b3c25ef3b623984fc194e849f61f0162
Volodymyr Senyk <vova.senyk@gmail.com>
2023-12-27 18:12:20 +0200

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App min-h-[100vh] bg-neutral-50">
+      <header className="App-header text-center bg-zinc-200 py-8">
+        <div className="container">
+          <h1 className="text-6xl font-bold text-sky-900">todos</h1>
+        </div>
       </header>
+      <main id="main" className="container flex-grow"></main>
+      <footer className="bg-zinc-900 text-white py-4 text-sm text-center font-mono">
+        <div className="container">
+          <span>Made with React {`</>`}</span>
+        </div>
+      </footer>
     </div>
   );
 }
